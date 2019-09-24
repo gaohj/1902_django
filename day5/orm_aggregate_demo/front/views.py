cc0c822ee8d4cbef4f52f6b7e0a4b78334833218
gaohj <gaohj@126.com>
2019-09-24 18:07:01 +0800

    FQ

--- day5/orm_aggregate_demo/front/views.py
+++ day5/orm_aggregate_demo/front/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book,Author,BookOrder,Publisher
-from django.db.models import Avg,Count,Max,Min,Sum
+from django.db.models import Avg,Count,Max,Min,Sum,F,Q
 from django.db import connection
 #获取所有图书的定价的平均价格
 def index(request):
@@ -36,3 +36,58 @@ def index2(request):
         print("%s/%s" % (res.name,res.booksales))
     print(connection.queries)
     return HttpResponse("index2")
+
+def index3(request):
+    #每一本书销售的时候最大价格和最小价格
+    result = Book.objects.annotate(max=Max("bookorder__price"),min=Min("bookorder__price"))
+    for res in result:
+        print("%s/%s/%s" %(res.name,res.max,res.min))
+    return HttpResponse("index3")
+
+def index4(request):
+    #所有图书销售的总额
+    # result = BookOrder.objects.aggregate(total=Sum("price"))
+    # print(result)
+
+    #每一本书销售的总额
+    # result = Book.objects.annotate(total=Sum("bookorder__price"))
+    # for res in result:
+    #     print("%s/%s" %(res.name,res.total))
+
+    #2018年度销售总额
+    # result = BookOrder.objects.filter(create_time__year=2018).aggregate(total=Sum("price"))
+    # print(result)
+
+    #每一本书2018年度销售的总额
+    books = Book.objects.filter(bookorder__create_time__year=2018).annotate(total=Sum("bookorder__price"))
+    for book in books:
+        print("%s/%s" %(book.name,book.total))
+    print(connection.queries)
+    return HttpResponse("index4")
+
+def index5(request):
+    #每一本图书的售价 增加10块钱
+    # books = Book.objects.all()
+    # for book in books:
+    #     book.price+=10
+    #     book.save()
+    # Book.objects.update(price=F("price")-10)
+    # print(connection.queries[-1])
+    #查询用户名和邮箱相等的 作者
+    authors = Author.objects.filter(name=F("email"))
+    for author in authors:
+        print("%s/%s" %(author.name,author.email))
+    print(connection.queries[-1])
+    return HttpResponse("index5")
+
+
+def index6(request):
+    #价格大于95并且评分大于4.5
+    # books = Book.objects.filter(price__gt=95,rating__gt=4.5)
+    #books = Book.objects.filter(Q(price__gt=95)&Q(rating__gt=4.5))
+    #如果需求是或者 办不了了
+    #~表示取反
+    books = Book.objects.filter(Q(price__gte=95)&~Q(name__icontains="传"))
+    for book in books:
+        print("%s/%s/%s" %(book.name,book.price,book.rating))
+    return HttpResponse("index6")
--- day5/orm_aggregate_demo/orm_aggregate_demo/urls.py
+++ day5/orm_aggregate_demo/orm_aggregate_demo/urls.py
@@ -21,4 +21,8 @@ urlpatterns = [
     path('', views.index),
     path('index1/', views.index1),
     path('index2/', views.index2),
+    path('index3/', views.index3),
+    path('index4/', views.index4),
+    path('index5/', views.index5),
+    path('index6/', views.index6),
 ]
